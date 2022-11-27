61a0b838d4cf71edcc0832fe2b4d6b6fad7be107
Toy Rik <toyrik@gmail.com>
2022-11-27 21:11:44 +0300

    Выносим повторяющиеся части шаблонов в отдельные файлы

--- resources/views/catalog/brand.blade.php
+++ resources/views/catalog/brand.blade.php
@@ -7,20 +7,7 @@
 
     <div class="row">
         @foreach ($products as $product)
-            <div class="col-md-6 mb-4">
-                <div class="card">
-                    <div class="card-header">
-                        <h4>{{ $product->name }}</h4>
-                    </div>
-                    <div class="card-body p-0">
-                        <img src="https://via.placeholder.com/400x120" alt="" class="img-fluid">
-                    </div>
-                    <div class="card-footer">
-                        <a href="{{ route('catalog.product', ['slug' => $product->slug]) }}"
-                           class="btn btn-dark">Перейти к товару</a>
-                    </div>
-                </div>
-            </div>
+            @include('catalog.part.product', ['product' => $product])
         @endforeach
     </div>
 @endsection
