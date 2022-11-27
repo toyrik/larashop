21b5e3b760b65549040f8c34b088bc80044c1f87
Toy Rik <toyrik@gmail.com>
2022-11-27 20:58:35 +0300

    Стилизация шаблонов вывода

--- resources/views/catalog/brand.blade.php
+++ resources/views/catalog/brand.blade.php
@@ -1,14 +1,26 @@
 @extends('layout.site')
 
 @section('content')
-    <h1>Бренд: {{ $brand->name }}</h1>
-    <ul>
+    <h1>{{ $brand->name }}</h1>
+
+    <p>{{ $brand->content }}</p>
+
+    <div class="row">
         @foreach ($products as $product)
-            <li>
-                <a href="{{ route('catalog.product', ['slug' => $product->slug]) }}">
-                    {{ $product->name }}
-                </a>
-            </li>
+            <div class="col-md-6 mb-4">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>{{ $product->name }}</h4>
+                    </div>
+                    <div class="card-body p-0">
+                        <img src="https://via.placeholder.com/400x120" alt="" class="img-fluid">
+                    </div>
+                    <div class="card-footer">
+                        <a href="{{ route('catalog.product', ['slug' => $product->slug]) }}"
+                           class="btn btn-dark">Перейти к товару</a>
+                    </div>
+                </div>
+            </div>
         @endforeach
-    </ul>
+    </div>
 @endsection
